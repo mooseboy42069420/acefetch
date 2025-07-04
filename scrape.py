3d5d9e5b8a8cf8f98f5f5b66d39185c10b51eaf6
Moose <>
2025-07-04 12:20:49 +0800

    cleanup

--- scrape.py
+++ scrape.py
@@ -14,7 +14,13 @@ REQUESTS_TIMEOUT = 10
 API_URL = "https://api.acestream.me/all?api_version=1&api_key=test_api_key"
 LOGOS_PATH = Path("channel_logos.xml")
 
-REMOVE_COUNTRY_CODE_REGEX = re.compile(r"\s*\[\w{2}\]\s*$")
+FIND_COUNTRY_CODE_REGEX = re.compile(r"\s*\[\w{2}\]\s*$")
+
+TVG_ID_COUNTRY_CODE_REGEX_1 = re.compile(r"\.(\w{2})\s*$")  # Matches .uk
+TVG_ID_COUNTRY_CODE_REGEX_2 = re.compile(r"^(\w{2})[ :]")  # Matches "UK " or "UK: "
+
+TVG_LOGO_REGEX = re.compile(r'tvg-logo="([^"]+)"')
+TVG_ID_REGEX = re.compile(r'tvg-id="([^"]+)"')
 
 ACE_URL_PREFIXES_CONTENT_ID = [
     "acestream://",
@@ -29,18 +35,31 @@ ACE_URL_PREFIXES_INFOHASH = [
     "http://127.0.0.1:6878/ace/manifest.m3u8?infohash=",
 ]
 
+SPORT_WORDS = [
+    "football",
+    "soccer",
+    "basketball",
+    "nba",
+    "sports",
+    "sport",
+    "tennis",
+]
 
+
+# region Classes
 @dataclass
 class Channel:
     """Object representing a channel."""
 
     name: str
     tvg_logo: str
+    tvg_id: str = ""
     infohash: str = ""
     content_id: str = ""
     category: str = ""
 
 
+# region get_logos
 def get_logos() -> dict:
     with LOGOS_PATH.open("r", encoding="utf-8") as file:
         logos_xml = file.read()
@@ -66,10 +85,11 @@ def get_logos() -> dict:
     return logos
 
 
+# region Names
 def find_best_match(name, logos) -> str:
     """Find the best matching logo for a given channel name."""
     # Remove any country code from the name, indicated by a two-letter suffix between square brackets
-    name = REMOVE_COUNTRY_CODE_REGEX.sub("", name.strip())
+    name = FIND_COUNTRY_CODE_REGEX.sub("", name.strip())
 
     # Use fuzzy matching to find the best match
     match = process.extractOne(
@@ -82,19 +102,6 @@ def find_best_match(name, logos) -> str:
     if match:
         return logos[match[0]]
     return ""
-
-
-def get_name_replacements(replacements_csv: Path) -> dict[str, str]:
-    """Get name replacements from a CSV file."""
-    if not replacements_csv.exists():
-        print(f"Replacements file {replacements_csv} does not exist!")
-        return {}
-
-    with replacements_csv.open("r", encoding="utf-8") as csvfile:
-        reader = csv.reader(csvfile)
-        replacements = {row[0]: row[1] for row in reader if len(row) == 2}
-
-    return replacements
 
 
 def do_name_replace(name: str, replacements: dict[str, str]) -> str:
@@ -112,6 +119,7 @@ def do_name_replace(name: str, replacements: dict[str, str]) -> str:
     return new_name.strip()
 
 
+# region File Wrangling
 def get_filter_list(filename: Path) -> list[str]:
     """Get a list of filters from a file."""
     if not filename.exists():
@@ -125,6 +133,65 @@ def get_filter_list(filename: Path) -> list[str]:
     return filters
 
 
+def get_name_replacements(replacements_csv: Path) -> dict[str, str]:
+    """Get name replacements from a CSV file."""
+    if not replacements_csv.exists():
+        print(f"Replacements file {replacements_csv} does not exist!")
+        return {}
+
+    with replacements_csv.open("r", encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        replacements = {row[0]: row[1] for row in reader if len(row) == 2}
+
+    return replacements
+
+
+# region TVG Handling
+def get_country_code_from_tvg_id(tvg_id: str) -> str:
+    """Extract country code from the tvg_id."""
+
+    matches = TVG_ID_COUNTRY_CODE_REGEX_1.findall(tvg_id)
+    if matches:
+        return f"[{matches[0].upper()}]"
+
+    matches = TVG_ID_COUNTRY_CODE_REGEX_2.findall(tvg_id)
+    if matches:
+        return f"[{matches[0].upper()}]"
+
+    return "[?]"
+
+
+def get_tvg_id_from_title(title: str) -> str:
+    """Extract the TVG ID from the title."""
+    country_code_regex = FIND_COUNTRY_CODE_REGEX.search(title)
+    if not country_code_regex:
+        return ""
+
+    try:
+        country_code_regex.group(0)
+    except IndexError:
+        print(f"Very strange title: {title}")
+        return ""
+
+    if isinstance(country_code_regex.group(0), str):
+        country_code = (
+            country_code_regex.group(0).replace("[", "").replace("]", "").strip()
+        )
+        title_no_cc = title.replace(f"[{country_code}]", "").strip()
+
+        return f"{title_no_cc}.{country_code.lower()}"
+    return ""
+
+
+def is_sport_channel(channel_name: str) -> bool:
+    """Check if a channel is a sport channel based on its name."""
+    # Normalize the channel name to lowercase for case-insensitive comparison
+    normalized_name = channel_name.lower()
+    # Check if any of the sport words are in the channel name
+    return any(sport_word.lower() in normalized_name for sport_word in SPORT_WORDS)
+
+
+# region Playlist
 def create_playlists(playlist_name: str, list_of_channels: list[Channel]) -> None:
     """Create M3U playlist file."""
     output_directory = Path("playlists")
@@ -142,7 +209,7 @@ def create_playlists(playlist_name: str, list_of_channels: list[Channel]) -> Non
         with playlist_path.open("w", encoding="utf-8") as m3u_file:
             m3u_file.write("#EXTM3U\n")
             for channel in list_of_channels:
-                top_line = f'#EXTINF:-1 tvg-logo="{channel.tvg_logo}" group-title="{channel.category}", {channel.name}\n'
+                top_line = f'#EXTINF:-1 tvg-logo="{channel.tvg_logo}" tvg-id="{channel.tvg_id}" group-title="{channel.category}", {channel.name}\n'
                 if channel.infohash != "" and uri_scheme == "local_infohash":
                     m3u_file.write(top_line)
                     m3u_file.write(f"{uri_schemes[uri_scheme]}{channel.infohash}\n")
@@ -151,6 +218,7 @@ def create_playlists(playlist_name: str, list_of_channels: list[Channel]) -> Non
                     m3u_file.write(f"{uri_schemes[uri_scheme]}{channel.content_id}\n")
 
 
+# region URL Handling
 def extract_infohash_from_url(url: str) -> str:
     """Extract infohash from a URL."""
     for prefix in ACE_URL_PREFIXES_INFOHASH:
@@ -167,6 +235,7 @@ def extract_content_id_from_url(url: str) -> str:
     return ""
 
 
+# region Download/API
 def populate_list_from_m3u(url: str) -> list[Channel]:
     """Populate a list of channels from an M3U file."""
     response = requests.get(url, timeout=REQUESTS_TIMEOUT)
@@ -185,8 +254,12 @@ def populate_list_from_m3u(url: str) -> list[Channel]:
             channel_name = extinf_parts[1].strip()
 
             # Extract logo URL if available
-            logo_match = re.search(r'tvg-logo="([^"]+)"', channel_info)
+            logo_match = TVG_LOGO_REGEX.search(channel_info)
             logo_url = logo_match.group(1) if logo_match else ""
+
+            # Extract tvg_id if available
+            tvg_id_match = TVG_ID_REGEX.search(channel_info)
+            tvg_id = tvg_id_match.group(1) if tvg_id_match else ""
 
             # Extract infohash from the next line
             if i + 1 < len(lines):
@@ -198,6 +271,7 @@ def populate_list_from_m3u(url: str) -> list[Channel]:
                     Channel(
                         name=channel_name,
                         tvg_logo=logo_url,
+                        tvg_id=tvg_id,
                         infohash=infohash,
                         content_id=content_id,
                     )
@@ -290,6 +364,12 @@ def main() -> None:
 
     for channel in channel_list_scratch:
         # Replace channel names if replacements are provided
+
+        if not FIND_COUNTRY_CODE_REGEX.search(channel.name):
+            channel.name = (
+                f"{channel.name} {get_country_code_from_tvg_id(channel.tvg_id)}"
+            )
+
         if name_replacements:
             channel.name = do_name_replace(channel.name, name_replacements)
 
@@ -298,6 +378,12 @@ def main() -> None:
             continue
 
         channel.tvg_logo = find_best_match(channel.name, logos)
+
+        if not channel.tvg_id:
+            channel.tvg_id = get_tvg_id_from_title(channel.name)
+
+        if is_sport_channel(channel.name):
+            channel.category = "sport"
 
         channel_list.append(channel)
 
